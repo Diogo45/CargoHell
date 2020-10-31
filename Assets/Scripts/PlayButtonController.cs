4a988b5e7450cfc2fefab6a4110ab2712ab2f848
Julia <julia.melgare@acad.pucrs.br>
2020-10-31 00:44:42 -0300

    working play button

---
+++ Assets/Scripts/PlayButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonController : MonoBehaviour
+{
+    public Button playButton;
+
+    void Start()
+    {
+        Button btn = playButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene("TestLevel");
+    }
+}
