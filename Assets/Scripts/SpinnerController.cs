e35e7bd3c9107fa3d5379ebc0e1900e385598211
Diogo45 <diogohartmann45@gmail.com>
2021-01-19 11:28:11 -0300

    spinner fix

--- Assets/Scripts/SpinnerController.cs
+++ Assets/Scripts/SpinnerController.cs
@@ -8,15 +8,31 @@ public class SpinnerController : IObject
 
     public float rotationSpeed;
     private float repeat = 1;
+    private bool hasCollided;
     void Start()
     {
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ProjectileReflected" || collision.gameObject.tag == "Projectile")
+        {
+            if (!hasCollided)
+            {
+                health--;
+                Destroy(collision.gameObject);
+                hasCollided = true;
+            }
+        }
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
-
+        base.Update();
         transform.position += (direction * speed) * Time.deltaTime;
 
         transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
